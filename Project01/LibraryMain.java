7962242db667bc6ae771a97a5d4ff067573609d9
Choi DeokJae <88370895+cdj6995@users.noreply.github.com>
2022-04-09 01:22:08 -0700

    inmemory방식

---
+++ Project01/LibraryMain.java
@@ -0,0 +1,14 @@
+package Project01;
+
+public class LibraryMain {
+
+	public static void main(String[] args) { 
+		Libraryview person = new Libraryview();
+		
+		// 도서관 프로그램 실행
+		person.playLibraryProgram();
+		
+		
+
+	}
+}
